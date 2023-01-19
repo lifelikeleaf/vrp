a05f2462683c8a04d9ff8296361873bd0fc26ff1
Leif Ye <82606859+lifelikeleaf@users.noreply.github.com>
2023-01-19 20:17:19 +0100

    remove obsolete get best found code; use constants; add wait time to no decomp results

--- experiments.py
+++ experiments.py
@@ -14,11 +14,9 @@ from vrp.decomp.decomposers import (
 from vrp.decomp.solvers import HgsSolverWrapper
 import vrp.decomp.helpers as helpers
 from vrp.decomp.logger import logger
+from vrp.decomp.constants import *
 
 logger = logger.getChild(__name__)
-
-SOLOMON = 'Vrp-Set-Solomon' # n=100; 56 instances
-HG = 'Vrp-Set-HG' # n=[200, 400, 600, 800, 1000]; 60 instances each
 
 
 class Experiment:
@@ -46,85 +44,12 @@ class ExperimentRunner:
         self.experiments.extend(experiments)
 
 
-    def repeat(self, num_clusters):
-        best_found_local = {}
-        best_found_local['cost'] = float('inf')
-
-        # repeat n times bc clustering algorithm may find diff clusters on each run
-        for _ in range(0, self.repeat_n_times):
-            self.decomp_runner.decomposer.num_clusters = num_clusters
-            solution = self.decomp_runner.run(in_parallel=True, num_workers=num_clusters)
-            cost = solution.metrics['cost']
-
-            # only tracking best found may not be a good idea
-            # bc cost could be 0 if no feasible soution found
-            # which would become the "best found solution"
-            if cost < best_found_local['cost']:
-                best_found_local['cost'] = cost
-                best_found_local['routes'] = solution.routes
-                best_found_local['num_clusters'] = num_clusters
-
-            # let the CPU take a break
-            helpers.sleep(3, __name__)
-
-        return best_found_local
-
-
-    def run_clusters_range(self):
-        best_found = {}
-        best_found['cost'] = float('inf')
-
-        # try clustering with diff number of clusters
-        min_clusters, max_clusters = self.num_clusters_range
-        for num_clusters in range(min_clusters, max_clusters + 1):
-            best_found_local = self.repeat(num_clusters)
-
-            if best_found_local['cost'] < best_found['cost']:
-                best_found['cost'] = best_found_local['cost']
-                best_found['routes'] = best_found_local['routes']
-                best_found['num_clusters'] = best_found_local['num_clusters']
-
-        return best_found
-
-
-    def get_decomp_best_found(self, instance_name, experiment_name):
-        cost_key = f'cost_{experiment_name}'
-        num_routes_key = f'num_routes_{experiment_name}'
-        num_subprobs_key = f'num_subprobs_{experiment_name}'
-
-        best_found = self.run_clusters_range()
-
-        sol_header = 'Solution - ' + experiment_name
-        logger.info(f"--------------- {sol_header} ---------------")
-        logger.info(f"Best decomp cost: {best_found['cost']} with "
-                    f"{best_found['num_clusters']} clusters and {len(best_found['routes'])} routes")
-        logger.info('')
-        for i, route in enumerate(best_found['routes']):
-            logger.debug(f"Route {i}: \n{route}")
-        logger.info('')
-
-        excel_data = {
-            'instance_name': [instance_name],
-            num_subprobs_key: best_found['num_clusters'],
-            num_routes_key: len(best_found['routes']),
-            cost_key: best_found['cost'],
-        }
-        df = pd.DataFrame(excel_data)
-        helpers.write_to_excel(df, self.output_file_name, experiment_name)
-
-        return excel_data
-
-
     def get_all_decomp(self, instance_name, experiment_name):
         '''write/log all results, not just best found'''
 
         logger.info('')
-        exp_header = 'Running experiment: ' + experiment_name
+        exp_header = f'Running experiment: {experiment_name} on instance {instance_name}'
         logger.info(f"--------------- {exp_header} ---------------")
-
-        cost_key = f'cost_{experiment_name}'
-        cost_wait_key = f'cost_wait_{experiment_name}'
-        num_routes_key = f'num_routes_{experiment_name}'
 
         # try clustering with diff number of clusters
         min_clusters, max_clusters = self.num_clusters_range
@@ -133,11 +58,11 @@ class ExperimentRunner:
             for i in range(self.repeat_n_times):
                 self.decomp_runner.decomposer.num_clusters = num_clusters
                 solution = self.decomp_runner.run(in_parallel=True, num_workers=num_clusters)
-                cost = solution.metrics['cost']
-                cost_wait = cost + solution.metrics['wait_time']
+                cost = solution.metrics[METRIC_COST]
+                cost_wait = cost + solution.metrics[METRIC_WAIT_TIME]
                 routes = solution.routes
 
-                sol_header = 'Solution for experiment: ' + experiment_name
+                sol_header = f'Solution for experiment: {experiment_name} on instance {instance_name}'
                 logger.info(f"------ {sol_header} ------")
                 logger.info(f"Decomp cost: {cost} (include wait time cost: {cost_wait}) with "
                             f"{num_clusters} clusters and {len(routes)} routes")
@@ -145,26 +70,29 @@ class ExperimentRunner:
 
                 # write KPIs to excel
                 excel_data = {
-                    'instance_name': [instance_name],
-                    'iteration': [i],
-                    'num_subprobs': [num_clusters],
-                    num_routes_key: [len(routes)],
-                    cost_key: [cost],
-                    cost_wait_key: [cost_wait],
+                    KEY_INSTANCE_NAME: [instance_name],
+                    KEY_ITERATION: [i],
+                    KEY_NUM_SUBPROBS: [num_clusters],
+                    KEY_NUM_ROUTES: [len(routes)],
+                    KEY_COST: [cost],
+                    KEY_COST_WAIT: [cost_wait],
                 }
                 df = pd.DataFrame(excel_data)
-                helpers.write_to_excel(df, self.output_file_name, experiment_name)
+                helpers.write_to_excel(df, self.output_file_name, sheet_name=experiment_name)
 
                 # write detailed routes to json
-                # routes_key = f'{instance_name}_{experiment_name}_{num_clusters}_{i}'
+                ## routes_key = f'{instance_name}_{experiment_name}_{num_clusters}_{i}'
                 json_data = {
-                    'instance_name': instance_name,
-                    'experiment_name': experiment_name,
-                    'num_subprobs': num_clusters,
-                    'iteration': i,
-                    'routes': routes,
+                    KEY_INSTANCE_NAME: instance_name,
+                    KEY_EXPERIMENT_NAME: experiment_name,
+                    KEY_NUM_SUBPROBS: num_clusters,
+                    KEY_ITERATION: i,
+                    KEY_ROUTES: routes,
                 }
                 helpers.write_to_json(json_data, self.output_file_name)
+
+                # let the CPU take a break
+                helpers.sleep(3, __name__)
 
 
     def run_experiments(self, inst):
@@ -177,7 +105,6 @@ class ExperimentRunner:
                 self.decomp_runner.inst = inst
                 self.decomp_runner.decomposer = experiment.decomposer
 
-            # self.get_decomp_best_found(inst.extra['name'], experiment.name)
             self.get_all_decomp(inst.extra['name'], experiment.name)
 
 
@@ -185,7 +112,7 @@ class ExperimentRunner:
         logger.info('')
         logger.info(f'Benchmark instance name: {instance_name}')
 
-        file_name = os.path.join('CVRPLIB', dir_name, instance_name)
+        file_name = os.path.join(CVRPLIB, dir_name, instance_name)
         inst, bk_sol = cvrplib.read(
             instance_path=f'{file_name}.txt',
             solution_path=f'{file_name}.sol'
@@ -202,9 +129,16 @@ class ExperimentRunner:
         # call solver directly without decomposition
         logger.info('')
         solution = self.solver.solve(inst)
-        cost = solution.metrics['cost']
-        logger.info(f'No decomp cost: {cost} with {len(solution.routes)} routes')
-        return cost, solution.routes
+        cost = solution.metrics[METRIC_COST]
+        cost_wait = cost + solution.metrics[METRIC_WAIT_TIME]
+        logger.info(f'No decomp cost: {cost} (include wait time cost: {cost_wait}) with {len(solution.routes)} routes')
+        json_data = {
+            KEY_INSTANCE_NAME: inst.extra['name'],
+            KEY_EXPERIMENT_NAME: 'No decomp',
+            KEY_ROUTES: solution.routes,
+        }
+        helpers.write_to_json(json_data, self.output_file_name)
+        return cost, cost_wait, solution.routes
 
 
     def run(self):
@@ -214,27 +148,25 @@ class ExperimentRunner:
             for instance_name in benchmark:
                 inst, bk_sol = self.read_instance(dir_name, instance_name)
                 converted_inst = helpers.convert_cvrplib_to_vrp_instance(inst)
-                no_decomp_cost, no_decomp_routes = self.get_no_decomp_solution(converted_inst)
+                no_decomp_cost, no_decomp_cost_wait, no_decomp_routes = self.get_no_decomp_solution(converted_inst)
 
                 # prepare data to be written to excel
                 excel_data = {
-                    'instance_name': [instance_name],
-                    'num_routes_BK': [len(bk_sol.routes)],
-                    'num_routes_NO_decomp': [len(no_decomp_routes)],
-                    'cost_BK': [bk_sol.cost],
-                    'cost_NO_decomp': [no_decomp_cost],
+                    KEY_INSTANCE_NAME: [instance_name],
+                    f'{KEY_NUM_ROUTES}_BK': [len(bk_sol.routes)],
+                    f'{KEY_NUM_ROUTES}_NO_decomp': [len(no_decomp_routes)],
+                    f'{KEY_COST}_BK': [bk_sol.cost],
+                    f'{KEY_COST}_NO_decomp': [no_decomp_cost],
+                    f'{KEY_COST_WAIT}_NO_decomp': [no_decomp_cost_wait],
                 }
-
-                # for data in decomp_data:
-                #     excel_data.update(data)
 
                 # write base reference data to excel in its own tab
                 # subsequently each experiment will also write its output
                 # in its own tab - one tab per experiment, one row per instance
                 df = pd.DataFrame(excel_data)
-                # df = df.reindex(sorted(df.columns), axis=1)
-                # sheet_name = f'{dir_name}-{size}'
-                helpers.write_to_excel(df, self.output_file_name, 'Basis')
+                ## df = df.reindex(sorted(df.columns), axis=1)
+                ## sheet_name = f'{dir_name}-{size}'
+                helpers.write_to_excel(df, self.output_file_name, sheet_name='Basis')
 
                 # run all the decomposition experiments on current VRP instance
                 self.run_experiments(converted_inst)
@@ -259,8 +191,17 @@ if __name__ == "__main__":
     # run on a deterministic set of instances rather than a random sample
     # so that new experiments can be compared to old ones w/o rerunning old ones
     # focus on the 1k nodes benchmark where decomp is important
-    # TODO: use Solomon 100-node benchmark first
 
+    # But use Solomon 100-node benchmark first for faster experiments
+    '''Solomon 100-node benchmark first'''
+    C1 = ['C101', 'C102', 'C103', 'C104', 'C105', 'C106', 'C107', 'C108', 'C109']
+    C2 = ['C201', 'C202', 'C203', 'C204', 'C205', 'C206', 'C207', 'C208']
+    R1 = ['R101', 'R102', 'R103', 'R104', 'R105', 'R106', 'R107', 'R108', 'R109', 'R110', 'R111', 'R112']
+    R2 = ['R201', 'R202', 'R203', 'R204', 'R205', 'R206', 'R207', 'R208', 'R209', 'R210', 'R211']
+    RC1 = ['RC101', 'RC102', 'RC103', 'RC104', 'RC105', 'RC106', 'RC107', 'RC108']
+    RC2 = ['RC201', 'RC202', 'RC203', 'RC204', 'RC205', 'RC206', 'RC207', 'RC208']
+
+    '''HG 1k-node benchmark'''
     ## geographically clustered
     ## narrow TWs
     C1_10 = ['C1_10_1', 'C1_10_2', 'C1_10_3', 'C1_10_4', 'C1_10_5', 'C1_10_6', 'C1_10_7', 'C1_10_8', 'C1_10_9', 'C1_10_10']
@@ -283,9 +224,10 @@ if __name__ == "__main__":
         experiments = []
         experiments.append(Experiment('euclidean', KMedoidsDecomposer()))
         experiments.append(Experiment('TW', KMedoidsDecomposer(use_tw=True)))
-        # # gap by default is negative
+        # # gap by default is negative (gap < 0)
         # experiments.append(Experiment('TW_Gap', KMedoidsDecomposer(use_tw=True, use_gap=True)))
-        # # what if gap is always positive by turning on min wait time, even though wait time is not yet included in OF
+        # # currently no wait time in OF value, only wait time added post-routing
+        # # so this essentially makes all gap a penalty (gap > 0)
         # experiments.append(Experiment('TW_Pos_Gap', KMedoidsDecomposer(use_tw=True, use_gap=True, minimize_wait_time=True)))
 
         # # normalized version of above
@@ -301,14 +243,17 @@ if __name__ == "__main__":
     repeat_n_times = 1
     time_limit = 5
     experiments = k_medoids
-    file_name = experiments.__name__
+    file_name = experiments.__name__ + '_test'
+    # benchmarks = [(RC2, 100)]
 
     # sample_size = 10
     # instance_sizes = [100, 200, 400, 600, 800, 1000]
 
     benchmarks = [(['C101'], 100)]
+    # Example instance returning no feasible solution:
+    # benchmarks = [(['R1_6_1'], 600)]
     # benchmarks = sample_benchmarks(sample_size, instance_sizes)
-    # benchmarks = [(C1_10, 1000)]
+
     '''parameters for experiments'''
 
 
