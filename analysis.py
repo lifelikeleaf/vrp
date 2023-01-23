e4f05eb1fc8a9e2d9f14f16680d78ba9513978d9
Leif Ye <82606859+lifelikeleaf@users.noreply.github.com>
2023-01-23 18:36:58 +0100

    add additional rows for counting comparison results per column

--- analysis.py
+++ analysis.py
@@ -81,6 +81,9 @@ def conditional_formatting(dir_name, sub_dir, file_name):
     wb = xl.load_workbook(file_to_load)
 
     for sheet in wb:
+        # conditional color formatting:
+        # - highlight cell value < 0 green
+        # - highlight cell value b/t 0 and 0.01 yellow (i.e. b/t 0% and 1%)
         green_fill = PatternFill(bgColor='00FF00', fill_type='solid')
         yellow_fill = PatternFill(bgColor='FFFF00', fill_type='solid')
         less_than_rule = CellIsRule(operator='lessThan', formula=['0'], fill=green_fill)
@@ -94,13 +97,38 @@ def conditional_formatting(dir_name, sub_dir, file_name):
         sheet.conditional_formatting.add(range_string, less_than_rule)
         sheet.conditional_formatting.add(range_string, between_rule)
 
-        header = row1 = sheet[1]
-        for cell in header:
+        headers = sheet[1] # row 1
+        for cell in headers:
             if cell.value is not None and '%' in cell.value:
                 percent_col = sheet[cell.column_letter]
                 for i in range(1, len(percent_col)): # skip the header row
                     cell = percent_col[i]
                     cell.number_format = '0.00%'
+
+        # add additional rows for counting comparison results per column
+        # - how many have diffs < 0? (i.e. performed strictly better)
+        # - how many have diffs b/t 0 and 1? (i.e. performed almost the same)
+        # - how many have diffs <= 1? (i.e. performed almost the same or better)
+        cur_max_row = sheet.max_row
+        cols_gen = sheet.columns
+        # next(cols_gen) # skip the first column which contains instance names
+        for col in cols_gen:
+            cell = col[0]
+            if cell.value is not None:
+                col_letter = cell.column_letter
+                cell_loc_1 = f'{col_letter}{cur_max_row + 2}'
+                cell_loc_2 = f'{col_letter}{cur_max_row + 3}'
+                cell_loc_3 = f'{col_letter}{cur_max_row + 4}'
+                formula_range = f'{col_letter}{start_row}:{col_letter}{cur_max_row}'
+                if col_letter == 'A':
+                    sheet[cell_loc_1] = 'x<0'
+                    sheet[cell_loc_2] = '0<=x<=1'
+                    sheet[cell_loc_3] = 'x<=1'
+                else:
+                    sheet[cell_loc_1] = f'=COUNTIF({formula_range}, "<0")'
+                    sheet[cell_loc_2] = f'=COUNTIFS({formula_range}, ">=0", {formula_range}, "<=1")'
+                    sheet[cell_loc_3] = f'=COUNTIF({formula_range}, "<=1")'
+
 
     out = os.path.join(dir_name, sub_dir, f'formatted_{file_name}.xlsx')
     # make sure the output dir exists
